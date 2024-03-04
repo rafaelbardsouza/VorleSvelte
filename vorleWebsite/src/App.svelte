b8f0f30697e4e114e48bde8d0f775284184fbeae
RAFAEL BARBOSA <rafaelbardsouza@gmail,com>
2024-03-04 18:15:29 -0300

    Vorle WIP
    website base created

---
+++ vorleWebsite/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+    import LandingPage from "./Components/landingPage.svelte";
+	let PageHandler='LandingPage';
+	function goHome(){
+		PageHandler='LandingPage'
+	}
+</script>
+
+<main>
+	<header>
+		<input type="text" placeholder="o que você procura?"/>
+		<div>
+			<button on:click={goHome}>Vorle</button>
+			<button>Aplicações</button>
+			<button>Nuvoton</button>
+			<button>Parceiros</button>
+		</div>
+	</header>
+	{#if PageHandler=='LandingPage'}
+		<LandingPage/>
+	{/if}
+</main>
+
+<style>
+
+</style>
---
+++ vorleWebsite/src/Components/landingPage.svelte
@@ -0,0 +1,24 @@
+<script>
+let x=0;
+function SliderHandler(){
+    x++;
+    if(x>1){
+        x=0
+    }
+}   
+
+</script>
+
+<main>
+    <div>
+        {#if x == 0}
+            <h1>Suporte Nuvoton</h1>
+        {:else}
+            <h1>Design for your application</h1>
+        {/if}
+        <button on:click={SliderHandler}/>
+        <button on:click={SliderHandler}/>
+    </div>
+</main>
+
+<style></style>
